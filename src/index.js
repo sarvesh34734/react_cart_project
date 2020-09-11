43843ffb7374109f25227f16c8e15d22add26ae4
sarvesh vyas <sarveshvyas@MacBook-Pro.local>
2020-09-12 01:18:40 +0530

    env variables added

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import * as firebase from "firebase";
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDyyRlk9OkaiXyIfU-yfEZxxaBQvnMqRLA",
-  authDomain: "cart-e47ca.firebaseapp.com",
-  databaseURL: "https://cart-e47ca.firebaseio.com",
-  projectId: "cart-e47ca",
-  storageBucket: "cart-e47ca.appspot.com",
-  messagingSenderId: "3570926035",
-  appId: "1:3570926035:web:dbf8626315ab698ff02f26"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
