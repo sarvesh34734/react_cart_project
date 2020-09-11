fed3f7567881f7bc49350c410f7ef3dc2c19a30e
sarvesh vyas <sarveshvyas@MacBook-Pro.local>
2020-09-12 01:00:53 +0530

    final touch up

--- src/App.js
+++ src/App.js
@@ -2,73 +2,151 @@ import React from 'react';
 import CartItem from './CartItem'
 import Cart from "./Cart"
 import Navbar from "./Navbar"
+import * as firebase from 'firebase'
 
 class App extends React.Component {
 
   constructor(){
     super();
     this.state = {
-        products:[
-            {
-                price:990,
-                title:"Phone",
-                qty:1,
-                img:"https://images.unsplash.com/photo-1505156868547-9b49f4df4e04?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
-                id:1
-            },
-            {
-                price:9900,
-                title:"Television",
-                qty:1,
-                img:"https://images.unsplash.com/photo-1461151304267-38535e780c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
-                id:2
-            },
-            {
-                price:99000,
-                title:"Laptop",
-                qty:1,
-                img:"https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
-                id:3
-            },
-            {
-                price:99,
-                title:"Watch",
-                qty:1,
-                img:"https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
-                id:4
-            }
-        ]
+        products:[],
+        loading:true
     }
+    this.db = firebase.firestore();
 
 }
+
+// render firebase after our app gets mounted
+// componentDidMount(){
+  // console.log("mounted");
+  // firebase
+  //   .firestore()
+  //   .collection('Product')
+  //   .get()
+  //   .then((snapshot)=>{
+  //     console.log(snapshot);
+  //     snapshot.docs.map((doc)=>{
+  //       console.log(doc.data())
+  //     })
+
+  //     const products = snapshot.docs.map((doc)=>{
+  //       const data = doc.data();
+  //       data['id'] = doc.id;
+  //       return data;
+  //     })
+
+  //     this.setState({
+  //       products,
+  //       loading:false
+  //     })
+  //   })
+
+  //onsnapshot() attaches a listener to our app so if we make any changes in firebase it gets reflected in react app without refreshing it
+  componentDidMount(){
+  this
+    .db
+    .collection('Product')
+    // .where("price","==",999) // used to write query to filter data
+    // .where("title","==",'watch')
+    // .orderBy("price","desc")     // sorts according to price default ascending
+    .onSnapshot((snapshot) => {
+        console.log(snapshot);
+
+        snapshot.docs.map((doc) => {
+          console.log(doc.data());
+        })
+
+        const products = snapshot.docs.map((doc) => {
+            const data = doc.data();
+            data['id'] = doc.id;
+            return data;
+        })
+
+        this.setState({
+          products,
+          loading:false
+        })
+    })
+  }
+
+
+
+
 
 handleIncreasedQuantity = (product)=>{
     // console.log("Hey! Please increase the quantity",product);
     const {products} = this.state;
     const index = products.indexOf(product);
-    products[index].qty +=1;
-    this.setState({
-        products
-    })
+    // products[index].qty +=1;
+    // this.setState({
+    //     products
+    // })
+
+    // this.db
+    //   .collection('Product')
+
+    // get the reference to the document that is clicked
+    const docRef = this.db.collection("Product").doc(products[index].id);
+
+    // update the document
+    docRef
+      .update({
+        qty : products[index].qty + 1
+      })
+      .then(() => {
+        console.log('Updated Successfully');
+      })
+      .catch((error) => {
+        console.log("Error => ",error)
+      })
+      
 }
 
 handleDecreasedQuantity = (product)=>{
     const {products} = this.state;
     const index = products.indexOf(product);
     if(products[index].qty>1){
-        products[index].qty-=1;
+      
+      // reference to document
+      const docRef = this.db.collection("Product").doc(products[index].id);
+
+      // update the docref
+      docRef.update({
+        qty : products[index].qty - 1
+      })
+      .then(() => {
+        console.log("Product Updated Successfully")
+      })
+      .catch((error) => {
+        console.log("Error => ",error )
+      })
+
     }
-    this.setState({
-        products
-    })
+    // this.setState({
+    //     products
+    // })
 }
 
 handleDeleteProduct = (id) => {
     const {products} = this.state;
-    const items = products.filter((item)=> item.id!==id );
+    // const index = products.indexOf(id);
+    // const items = products.filter((item)=> item.id!==id );
 
-    this.setState({
-        products:items
+    // this.setState({
+    //     products:items
+    // })
+
+    // doc reference
+    const docRef = this.db.collection("Product").doc(id);
+
+    // delete
+    docRef
+      .delete()
+      .then(() => {
+        console.log("Deleted Sucessfully")
+    })
+    .catch((error) => {
+      console.log("Error => ",error)
     })
 }
 
@@ -90,17 +168,39 @@ getCartTotal = () => {
   return total;
 }
 
+// add product in firebase
+addProduct = () => {
+
+  this
+    .db
+    .collection('Product')
+    .add({
+      img : '',
+      price : 900,
+      qty : 3,
+      title : 'Washing Machine'
+    })
+    .then((docRef) => {
+      console.log("Product has been added : ",docRef)
+    })
+    .catch((error) => {
+      console.log("Error :: ",error);
+    })
+}
+
   render(){
-    const {products} = this.state;
+    const {products,loading} = this.state;
     return (
       <div className="App">
         <Navbar count = {this.getCartCount()}/>
+        {/* <button onClick={this.addProduct} style={{padding:20, fontSize:20}}> Add a Product</button> */}
         <Cart 
           products = {products}
           onIncreaseQuantity={this.handleIncreasedQuantity}
           onDecreaseQuantity = {this.handleDecreasedQuantity}
           onHandleDelete = {this.handleDeleteProduct}
         />
+        {loading && <h1>Loading Products ... </h1>}
         <div style={{padding:20,fontSize:20}}>TOTAL: {this.getCartTotal()}</div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from "firebase";
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDyyRlk9OkaiXyIfU-yfEZxxaBQvnMqRLA",
+  authDomain: "cart-e47ca.firebaseapp.com",
+  databaseURL: "https://cart-e47ca.firebaseio.com",
+  projectId: "cart-e47ca",
+  storageBucket: "cart-e47ca.appspot.com",
+  messagingSenderId: "3570926035",
+  appId: "1:3570926035:web:dbf8626315ab698ff02f26"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 
 ReactDOM.render(
